49d7343f17aaa67a936d99394f9043e5dcd83591
HeitorBraga <heitorbrasil62@gmail.com>
2023-05-11 14:34:48 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../node_modules/bulma/css/bulma.min.css'
+import '../styles/globals.css'
+
+function MyApp({ Component, PageProps }) {
+  return <Component {...PageProps} />
+}
+
+export default MyApp
